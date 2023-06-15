ba4a0bcb6fa9e6371994d77fe3564d1876684b05
Abhishek Saxena <saxenaabhishek7944@gmail.com>
2023-06-15 14:26:51 +0530

    Minor Updates

--- src/main/scala/Absolute_Basics/Functions.scala
+++ src/main/scala/Absolute_Basics/Functions.scala
@@ -30,14 +30,39 @@ object Functions extends App{
     else n*Factorial(n-1, accumulator)
   }
 
+
+
+
+
   def aBigFunction(n:Int) = {
     def aSmallFunction(a:Int, b:Int) = a + b
 
     aSmallFunction(n, n-1)
   }
 
+  /*DataType of aBigFunction is determined by aSmallFunction because it's returned here and gives int*/
+
   println(aBigFunction(10))
 
+
+
+
+  //Practice Questions
+  def greetings(name: String, age: Int) = s"Hi, my name is $name and I am $age years old"
+  println(greetings("Abhishek",24))
+
+
+  def Factorial(n:Int): Int = {
+    @tailrec
+    def factorial(num: Int, accumulator: Int): Int = {
+      if (num==1) accumulator
+      else factorial(num-1, accumulator*num)
+    }
+
+    factorial(n, 1)
+  }
+
+  println(Factorial(5))
 
 
 }
--- src/main/scala/Absolute_Basics/Recursion.scala
+++ src/main/scala/Absolute_Basics/Recursion.scala
@@ -22,6 +22,37 @@ object Recursion extends App {
 
 
 
+  def numberOfDigits(num:Int):Int = {
+    @tailrec
+    def totalDigits(n:Int, accumulator: Int):Int ={
+      if (n<= 1) accumulator
+      else totalDigits(n/10, accumulator + 1)
+    }
+
+    totalDigits(num, 1)
+
+
+  }
+
+  println(numberOfDigits(1345607))
+
+
+
+  def sumOfDigits(num:Int):Int = {
+    @tailrec
+    def totalSum(n:Int, accumulator:Int): Int ={
+      if (n==0) accumulator
+      else totalSum(n/10, n%10 + accumulator)
+    }
+
+    totalSum(num, 0)
+  }
+
+  println(sumOfDigits(123416))
+
+
+
+
 
 
 
