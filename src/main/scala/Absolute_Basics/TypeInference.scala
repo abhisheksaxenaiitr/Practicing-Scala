8cd4af9ff4316903385c4aed6571f1428b36b237
Abhishek Saxena <saxenaabhishek7944@gmail.com>
2023-06-15 16:24:52 +0530

    Minor Updates

--- src/main/scala/Absolute_Basics/TypeInference.scala
+++ src/main/scala/Absolute_Basics/TypeInference.scala
@@ -3,5 +3,13 @@ package Absolute_Basics
 object TypeInference extends App {
   val a = 3
   val b = "AbS"
+  val c = a + b                       //Int + String = String
+
+  def add(x:String) = x + 1           //Int + String = String
+  def add2(x:String) = x + "1"        //String + String
+  def add(x:Int) = x + 1                 //Int + Int
+  def add2(x:Int) = x + "1"           //Int + String = String
+
+/*In case of Recursion always mention the return type of function*/
 
 }
