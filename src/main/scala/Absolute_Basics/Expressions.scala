a40c09a1a2f7a82f5fdf11ca7bd484c82db8db23
Abhishek Saxena <saxenaabhishek7944@gmail.com>
2023-06-15 12:23:29 +0530

    Minor Updates

--- src/main/scala/Absolute_Basics/Expressions.scala
+++ src/main/scala/Absolute_Basics/Expressions.scala
@@ -2,13 +2,16 @@ package Absolute_Basics
 
 object Expressions extends App {
 
-  /*In Scala, an expression is a piece of code that produces a value when evaluated.
+  /*
+  In Scala, an expression is a piece of code that produces a value when evaluated.
   It can be a simple value, a function call, a mathematical operation like pow, or a combination of these. */
 
   //if expression
   val aCondition = true
   val aConditionedValue = if(aCondition) 5 else 3
   println(aConditionedValue)
+
+
 
 
   //while loop
@@ -18,6 +21,32 @@ object Expressions extends App {
     i+= 1             //Not i+ = 1 (No space between + and =)
     println(i)
   }
+
+  /*The above while loop returns Unit which shows the side effect
+   In Scala, Unit is a type that represents the absence of a meaningful value. It is similar to the void type in other
+   programming languages like Java or C. The Unit type is used to indicate that a function or expression does not return
+   a useful result.
+   Functions, Loops, Code Blocks that have a Unit return type are typically used for performing side effects, such as
+    printing to the console, updating state, or interacting with external resources.
+   */
+
+
+
+
+  //Code Block
+  val aCodeBlock = {
+    val x = 3
+    val y = "AbS"
+    if (x>2) "Yes" else "No"
+  }
+
+  /*For Code Block the datatype and value is taken from its last expression
+  and we can't use its variables outside the block ex- x & y can't be used outside block*/
+
+
+
+
+
 
   
 
--- src/main/scala/Absolute_Basics/ValuesVariablesTypes.scala
+++ src/main/scala/Absolute_Basics/ValuesVariablesTypes.scala
@@ -9,11 +9,46 @@ object ValuesVariablesTypes extends App {
   val f: Float = 1.01F                      //Put F to specify it's a Floating Type Number
   val g: Long = 124356767787L               //By Default Int so put L at the end to specify it's a Long
 
+
+
   /*-------------------------------------------------------------------------------------------------------------------
+  ##BigInt##
   In Scala, a BigInt is a built-in class that represents arbitrary-precision integers. It is part of the Scala standard
   library and provides a way to work with integers of any size, limited only by the available memory.*/
 
   val x: BigInt = 123456789
+  val y = BigInt("123456789012345")
+
+  /*The choice between the two constructors depends on the source of the number you want to represent as a BigInt. If
+   you have a regular integer value, you can use the first constructor. If you have a string representation of a large
+   number, you can use the second constructor.
+  It's worth noting that the string constructor is more flexible as it allows you to represent numbers beyond the range
+   of regular integer types, such as Int or Long. The string constructor can handle arbitrarily large numbers limited
+   only by the available memory.
+   -------------------------------------------------------------------------------------------------------------------*/
+
+
+  /*--------------------------------------------------------------------------------------------------------------------
+  Side effect: An observable change or interaction that occurs outside the primary purpose of a function or expression,
+  typically involving modifications to the external environment or state.
+
+  Here's a list of short examples of side effects:
+  Modifying a variable
+  Printing to the console
+  Writing to a file
+  Reading from a file
+  Making a network request
+  Updating a database
+  Modifying the user interface
+  Mutating an array or collection
+  Changing the state of an object
+  Throwing an exception
+  Logging information
+  Sleeping or delaying execution
+  Mutating shared or global state
+  Triggering external events or callbacks
+  Interacting with hardware or peripherals
+  ---------------------------------------------------------------------------------------------------------------------*/
 
 
 
