c9da0afefdc85f248980deaf740595a1a1d0d312
Abhishek Saxena <saxenaabhishek7944@gmail.com>
2023-06-22 12:43:30 +0530

    Updating Question

---
+++ src/main/scala/Leetcode_Question/fincdSubsequence.scala
@@ -0,0 +1,22 @@
+/*https://leetcode.com/problems/is-subsequence/?envType=study-plan-v2&envId=leetcode-75*/
+
+package Leetcode_Question
+
+import scala.annotation.tailrec
+
+object fincdSubsequence extends App {
+  def isSubsequence(s: String, t: String): Boolean = {
+    @tailrec
+    def isSubsequenceUtil(sIndex: Int, tIndex: Int): Boolean = {
+      if (sIndex >= s.length) true
+      else if (tIndex >= t.length) false
+      else if (s.charAt(sIndex) == t.charAt(tIndex))
+        isSubsequenceUtil(sIndex + 1, tIndex + 1)
+      else
+        isSubsequenceUtil(sIndex, tIndex + 1)
+    }
+
+    isSubsequenceUtil(0, 0)
+  }
+}
+
